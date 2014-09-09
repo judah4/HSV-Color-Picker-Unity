b620140460ed90803f28c8235f7bdb58c7717e3f
Judah Perez Enteracloud <tythin4@gmail.com>
2014-09-09 11:20:56 -0700

    Added contents
    Added hsvColorPicker to project

---
+++ HsvBoxSelector.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class HsvBoxSelector : MonoBehaviour, IDragHandler, IPointerDownHandler {
+
+    public HSVPicker picker;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PlaceCursor(PointerEventData eventData)
+    {
+
+        var pos = new Vector2(eventData.position.x - picker.hsvImage.rectTransform.position.x, eventData.position.y - picker.hsvImage.rectTransform.position.y + picker.hsvImage.rectTransform.rect.height);
+        // Debug.Log(pos);
+        pos.x /= picker.hsvImage.rectTransform.rect.width;
+        pos.y /= picker.hsvImage.rectTransform.rect.height;
+
+        pos.x = Mathf.Clamp(pos.x, 0, .9999f);  //1 is the same as 0
+        pos.y = Mathf.Clamp(pos.y, 0, .9999f);
+
+        //Debug.Log(pos);
+        picker.MoveCursor(pos.x, pos.y);
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        PlaceCursor(eventData);
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PlaceCursor(eventData);
+    }
+}
