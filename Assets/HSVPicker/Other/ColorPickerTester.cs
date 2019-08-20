cf65418f9bfc6dd81a4b056854afc25210b8b783
Judah Perez <judah@cookiedragon.games>
2019-08-20 13:44:13 -0700

    Picker Setting Example scene for setting colors

--- Assets/HSVPicker/Other/ColorPickerTester.cs
+++ Assets/HSVPicker/Other/ColorPickerTester.cs
@@ -18,10 +18,18 @@ public class ColorPickerTester : MonoBehaviour
         });
 
 		renderer.material.color = picker.CurrentColor;
+        Debug.Log($"Color: {Color} {ToColor32(Color)}");
 
-        picker.CurrentColor = Color;
+        picker.AssignColor(Color);
+
+        Debug.Log($"Picker Color: {picker.CurrentColor} {ToColor32(picker.CurrentColor)}");
     }
-	
+
+    Color32 ToColor32(Color color)
+    {
+        return new Color32((byte)(color.r * 255), (byte)(color.g * 255), (byte)(color.b * 255), (byte)(color.a * 255));
+    }
+
 	// Update is called once per frame
 	void Update () {
 	
