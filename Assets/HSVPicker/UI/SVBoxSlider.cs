1b3d4d0e2cc2db4bf93a773a9bde79a2ad333890
Alberto Vaccari <alberto.vaccari@mequilibrium>
2021-10-02 23:26:58 +0200

    Added On Change End event - Issue #46

--- Assets/HSVPicker/UI/SVBoxSlider.cs
+++ Assets/HSVPicker/UI/SVBoxSlider.cs
@@ -41,6 +41,7 @@ namespace HSVPicker
             {
                 slider.onValueChanged.AddListener(SliderChanged);
                 picker.onHSVChanged.AddListener(HSVChanged);
+                RegenerateSVTexture();
             }
         }
 
